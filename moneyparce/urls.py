9a76cee3b33b8d2f3a32276850c6b8c5d7685db1
FrancisWu2005 <88761237+FrancisWu2005@users.noreply.github.com>
2025-04-08 15:53:51 -0400

    Update urls.py

--- moneyparce/urls.py
+++ moneyparce/urls.py
@@ -19,4 +19,10 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('', include('transactions.urls')),
+    path('budgets/', include('budgets.urls')),
+    path('banks/', include('bank_sync.urls')),
+    path('reports/', include('reports.urls')),
+    #path('ai/', include('ai_advice.urls')),
 ]
