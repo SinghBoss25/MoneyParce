6b734ba5f823432c6a23769a95933d0a7998fccc
monabakare <monabakare@gmail.com>
2025-04-21 23:23:51 -0400

    fixed "Get started button" to register user or stay on homepage if not logged in

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home.index'),
     path('about', views.about, name='home.about'),
+    path('get-started/', views.get_started_redirect, name='get_started'),
 ]
