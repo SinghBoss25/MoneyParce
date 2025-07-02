cc81c20e8a618998b0bc87068bb9d2e7293728c9
Mannan Singh <123999838+SinghBoss25@users.noreply.github.com>
2025-07-02 08:46:25 +0000

    commit to github

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# Defines the URL patterns for the Home application, mapping each URL path
+# to its corresponding view function.
 urlpatterns = [
     path('', views.index, name='home.index'),
     path('about/', views.about, name='home.about'),
